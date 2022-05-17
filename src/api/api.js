3a9805dc566856f3b7f1ba256dae57e5175ca653
septoon <tigran.darchinyan@gmail.com>
2022-05-18 00:08:38 +0300

    api fixed

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const apiAddressKey = '431b14bdd798646063dbe3728107af4a'
 const apiWeatherKey = '056b91ad44e2ed8399949215e7ec5215'
 
 export const getAddressData = () => {
-  return axios.get(`http://api.ipstack.com/check?access_key=${apiAddressKey}`).then((response) => {
+  return axios.get(`http://api.ipstack.com/check/?access_key=${apiAddressKey}`).then((response) => {
     // console.log(response.data)
     return response.data
   })
@@ -12,7 +12,7 @@ export const getAddressData = () => {
 
 export const getTemperature = async () => {
   const address = await getAddressData()
-  return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${address.city}&units=metric&appid=${apiWeatherKey}`)
+  return axios.get(`https://api.openweathermap.org/data/2.5/weather/?q=${address.city}&units=metric&appid=${apiWeatherKey}`)
   .then(response => {
     // console.log(response.data)
     return response.data
