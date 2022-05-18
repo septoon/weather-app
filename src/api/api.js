53ac7c05a7e312ad7675849d2be56f93d7d02faa
septoon <tigran.darchinyan@gmail.com>
2022-05-18 11:51:44 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,8 @@ const apiAddressKey = '431b14bdd798646063dbe3728107af4a'
 const apiWeatherKey = '056b91ad44e2ed8399949215e7ec5215'
 
 export const getAddressData = () => {
-  return axios.get(`http://api.ipstack.com/check/?access_key=${apiAddressKey}`).then((response) => {
-    // console.log(response.data)
+  return axios.get(`https://api.ipstack.com/check/?access_key=${apiAddressKey}`).then((response) => {
+    console.log(response.data)
     return response.data
   })
 }
